6b338ea31d07757fe63e17ef09aee30f6a70ff84
Jiwon Choi <jiwon719choi@gmail.com>
2025-05-01 12:33:41 +0900

    java 중첩된 for 문

---
+++ src/ch04/lecture/C20For.java
@@ -0,0 +1,14 @@
+package ch04.lecture;
+
+public class C20For {
+    public static void main(String[] args) {
+        System.out.println("코드1");
+        System.out.println("코드2");
+        // 빨간 점은 break point 여기서 디버그 실행하면..
+        // 일시정지 하고 f8누르면 하나씩 다음 줄 실행
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
